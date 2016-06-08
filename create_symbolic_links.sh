71409b19127b7833dc42efd79e4fb2cec7d2efc3
Sergiu Bodiu <sbodiu@pivotal.io>
2016-06-08 16:55:30 +0800

    add create_symbolic_links

---
+++ create_symbolic_links.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+declare -a FILES_TO_SYMLINK=(
+
+    "shell/aliases"
+    "shell/bash_profile"
+    "shell/bashrc"
+    "shell/curlrc"
+    "shell/exports"
+    "shell/functions"
+    "shell/inputrc"
+    "shell/screenrc"
+    "shell/tmux.conf"
+    "shell/zshrc"
+
+    "git/gitattributes"
+    "git/gitconfig"
+    "git/gitignore"
+
+)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    local i=""
+    local sourceFile=""
+    local targetFile=""
+    local skipQuestions=false
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    while :; do
+        case $1 in
+            -y|--yes) skipQuestions=true; break;;
+                   *) break;;
+        esac
+        shift 1
+    done
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    for i in "${FILES_TO_SYMLINK[@]}"; do
+
+        sourceFile="$(pwd)/$i"
+        targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+        if [ ! -e "$targetFile" ] || $skipQuestions; then
+
+            execute \
+                "ln -fs $sourceFile $targetFile" \
+                "$targetFile → $sourceFile"
+
+        elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
+            print_success "$targetFile → $sourceFile"
+        else
+
+            if ! $skipQuestions; then
+
+                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+                if answer_is_yes; then
+
+                    rm -rf "$targetFile"
+
+                    execute \
+                        "ln -fs $sourceFile $targetFile" \
+                        "$targetFile → $sourceFile"
+
+                else
+                    print_error "$targetFile → $sourceFile"
+                fi
+
+            fi
+
+        fi
+
+    done
+
+}
+
+main "$@"
