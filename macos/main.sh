4c892593a7cb0e32eba3126daa8798199c765fcc
Sergiu Bodiu <sergiu.bodiu@gmail.com>
2020-03-31 18:41:36 +0800

    prepare centos setup

--- macos/main.sh
+++ macos/main.sh
@@ -17,7 +17,7 @@ main() {
 
     opt_out_of_analytics
 
-    brew_install 'Homebrew Cask' 'caskroom/cask/brew-cask' 'caskroom/cask'
+    brew_install 'Homebrew Cask' 'homebrew/cask/brew-cask' 'homebrew/cask'
 
     print_in_green '\n  ---\n\n'
 
@@ -31,40 +31,43 @@ main() {
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    brew_install 'Alfred' 'alfred' 'caskroom/cask' 'cask'
-    #brew_install 'F.lux' 'flux' 'caskroom/cask' 'cask'
-    brew_install 'Docker' 'docker' 'caskroom/cask' 'cask'
-    brew_install 'Intellij' 'intellij-idea-ce' 'caskroom/cask' 'cask'
-    brew_install 'Oracle JDK' 'java' 'caskroom/cask' 'cask'
-    brew_install 'LICEcap' 'licecap' 'caskroom/cask' 'cask'
-    brew_install 'Opera' 'opera' 'caskroom/cask' 'cask'
-    brew_install 'VirtualBox' 'virtualbox' 'caskroom/cask' 'cask'
+    brew_install 'Alfred' 'alfred' 'homebrew/cask' 'cask'
+    #brew_install 'F.lux' 'flux' 'homebrew/cask' 'cask'
+    brew_install 'Docker' 'docker' 'homebrew/cask' 'cask'
+    brew_install 'Intellij' 'intellij-idea-ce' 'homebrew/cask' 'cask'
+    brew_install 'LICEcap' 'licecap' 'homebrew/cask' 'cask'
+    brew_install 'Opera' 'opera' 'homebrew/cask' 'cask'    
+    brew_install 'Oracle JDK' 'java' 'homebrew/cask' 'cask'
+    brew_install 'Unarchiver' 'the-unarchiver' 'homebrew/cask' 'cask'
+    brew_install 'VirtualBox' 'virtualbox' 'homebrew/cask' 'cask'
+    brew_install 'Visual Studio Code' 'visual-studio-code' 'homebrew/cask' 'cask'
 
     brew_install 'Git' 'git'
     brew_install 'Gradle' 'gradle'
     brew_install 'Maven' 'maven'
+    brew_install 'ShellCheck' 'shellcheck'
     brew_install 'tmux' 'tmux'
     brew_install 'Tree' 'tree'
     brew_install 'watch' 'watch'
 
-    # brew_install 'Skype' 'skype' 'caskroom/cask' 'cask'
-    # brew_install 'Chrome' 'google-chrome' 'caskroom/cask' 'cask'
-    # brew_install 'Transmission' 'transmission' 'caskroom/cask' 'cask'
+    # brew_install 'Skype' 'skype' 'homebrew/cask' 'cask'
+    # brew_install 'Chrome' 'google-chrome' 'homebrew/cask' 'cask'
+    # brew_install 'Transmission' 'transmission' 'homebrew/cask' 'cask'
+    
 
-
-    # brew_install 'VLC' 'vlc' 'caskroom/cask' 'cask'
-    # brew_install 'ImageAlpha' 'imagealpha' 'caskroom/cask' 'cask'
-    # brew_install 'ImageMagick' 'imagemagick --with-webp'
-    # brew_install 'ImageOptim' 'imageoptim' 'caskroom/cask' 'cask'
+    # brew_install 'VLC' 'vlc' 'homebrew/cask' 'cask'
+    # brew_install 'ImageAlpha' 'imagealpha' 'homebrew/cask' 'cask'
+    # brew_install 'ImageMagick' 'imagemagick'
+    # brew_install 'ImageOptim' 'imageoptim' 'homebrew/cask' 'cask'
     # brew_install 'TTF/OTF → WOFF (Zopfli)' 'sfnt2woff-zopfli' 'bramstein/webfonttools'
     # brew_install 'TTF/OTF → WOFF' 'sfnt2woff' 'bramstein/webfonttools'
-    # brew_install 'Unarchiver' 'the-unarchiver' 'caskroom/cask' 'cask'
+    # brew_install 'Unarchiver' 'the-unarchiver' 'homebrew/cask' 'cask'
     # brew_install 'WOFF2' 'woff2' 'bramstein/webfonttools'
     # brew_install 'Zopfli' 'zopfli'
     
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #    brew_install 'AWS' 'awscli'
-#    brew_install 'Android File Transfer' 'android-file-transfer' 'caskroom/cask' 'cask'   
+#    brew_install 'Android File Transfer' 'android-file-transfer' 'homebrew/cask' 'cask'   
 #    brew_install 'CloudFoundry' 'cf-cli' 'cloudfoundry/tap'
 #    brew_install 'SpringBoot' 'springboot' 'pivotal/tap'
 #    brew install 'chruby' 'chruby'
--- utils.sh
+++ utils.sh
@@ -29,17 +29,19 @@ execute() {
     eval "$1" &> /dev/null
     print_result $? "${2:-$1}"
 }
-
+# http://0pointer.de/blog/projects/os-release
+# One of the new configuration files systemd introduced is /etc/os-release.
 get_os() {
 
+    local os=""
     local osName="$(uname -s)"
-    local os=''
 
-    if [[ "$osName" == "Darwin" ]]; then
+    if [ "$osName" == "Darwin" ]; then
         os='macos'
-    elif [[ "$osName" == "Linux" ]] && [ -e "/etc/lsb-release" ]; then
-        os='ubuntu'
-    elif [[ "$osName" == "MINGW64_NT-10.0" ]]; then
+    elif [ "$osName" == "Linux" ] && \
+         [ -e "/etc/os-release" ]; then
+        os="$(. /etc/os-release; printf "%s" "$ID")"
+    elif [ "$osName" == "MINGW64_NT-10.0" ]; then
         os='windows'
     else
         os="$osName"
